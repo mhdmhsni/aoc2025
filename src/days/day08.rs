872d9e010f29126e86ff60cbef5417634bc8b7e0
Mehdi Mohseni <mhdmhsnidev@gmail.com>
2025-12-07 12:39:54 +0100

    init day 8

---
+++ src/days/day08.rs
@@ -0,0 +1,15 @@
+/// Solve returns (part1, part2) as strings.
+pub fn solve(input: &str) -> (String, String) {
+    // TODO: implement
+    let part1 = solve_part1(input);
+    let part2 = solve_part2(input);
+    (part1, part2)
+}
+
+fn solve_part1(input: &str) -> String {
+    String::new()
+}
+
+fn solve_part2(input: &str) -> String {
+    String::new()
+}
